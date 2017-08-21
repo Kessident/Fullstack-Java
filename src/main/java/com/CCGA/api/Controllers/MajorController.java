427991ed463c963e14efad7722333b971bf5f6eb
windspinks <windspinks@gmail.com>
2017-08-21 11:08:15 -0400

    Major Controller

--- src/main/java/com/CCGA/api/Controllers/MajorController.java
+++ src/main/java/com/CCGA/api/Controllers/MajorController.java
@@ -1,7 +1,38 @@
 package com.CCGA.api.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.CCGA.api.Models.JSONResponse;
+import com.CCGA.api.Models.Major;
+import com.CCGA.api.Repositorys.MajorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MajorController {
+
+    @Autowired
+    MajorRepo majors;
+
+    @GetMapping("/all")
+    public JSONResponse getAllMajors(){
+        List<Major> majorList = new ArrayList<>();
+        majors.findAll().forEach(majorList::add);
+
+        return new JSONResponse("Success", majorList);
+    }
+
+    @GetMapping("{ID}")
+    public JSONResponse getAMajor(@RequestParam int majorID){
+        return new JSONResponse("Success", majors.findOne(majorID));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createSchool(@RequestBody Major major) {
+        majors.save(major);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
 }
